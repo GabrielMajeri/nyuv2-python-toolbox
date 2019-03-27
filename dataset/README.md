464d1d84f154980050b14a245bc9c872ee78a174
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-03-27 07:13:12 +0200

    Add test code for the labeled subset

---
+++ dataset/README.md
@@ -0,0 +1,11 @@
+# Dataset directory
+
+In order to run the tests for this library, download or symlink the files of
+the NYU V2 dataset here.
+
+More specifically, the files the test code will try to load are:
+
+- `nyu_depth_v2_labeled.mat` for the labeled subset
+
+- At least one part of the raw dataset, as `*.zip` files
+  (for example, download and place `cafe.zip` here)
---
+++ test.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from nyuv2 import LabeledDataset
+import numpy as np
+import matplotlib.pyplot as plt
+
+DATASET_DIR = Path('dataset')
+
+def plot_color(ax, color):
+    """Displays a color image from the NYU dataset."""
+
+    color = np.moveaxis(color, 0, -1)
+    ax.axis('off')
+    ax.set_title("Color")
+    ax.imshow(color)
+
+def plot_depth(ax, depth):
+    """Displays a depth map from the NYU dataset."""
+
+    ax.axis('off')
+    ax.set_title("Depth")
+    ax.imshow(depth, cmap='Spectral')
+
+def plot_color_depth(title, color, depth):
+    """Draws the color and depth maps of a scene, side-by-side."""
+
+    fig = plt.figure(title, figsize=(12, 5))
+
+    ax = fig.add_subplot(1, 2, 1)
+    plot_color(ax, color)
+
+    ax = fig.add_subplot(1, 2, 2)
+    plot_depth(ax, depth)
+
+    plt.show()
+
+def test_labeled_dataset():
+    labeled = LabeledDataset(DATASET_DIR / 'nyu_depth_v2_labeled.mat')
+
+    plot_color_depth("Labeled Dataset Sample", *labeled[42])
+
+    labeled.close()
+
+test_labeled_dataset()
